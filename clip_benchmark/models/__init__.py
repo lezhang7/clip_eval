1e7f2a5bd6123808d916ee5e213ed17641b839c8
Le Zhang <zhangle7@tamia2.tamia.ecpia.ca>
2025-04-16 19:52:54 -0400

    update model loading

--- clip_benchmark/models/__init__.py
+++ clip_benchmark/models/__init__.py
@@ -1,6 +1,8 @@
 from typing import Union
 import torch
-
+from .open_clip import load_open_clip
+from .japanese_clip import load_japanese_clip
+from .colxlip import load_colxlip
 
 # loading function must return (model, transform, tokenizer)
 TYPE2FUNC = {
@@ -26,7 +28,5 @@ def load_clip(
         from .sail import load_sail
         return load_func(model_name=model_name, pretrained=pretrained, cache_dir=cache_dir, device=device, sharelock=sharelock)
     else:
-        from .open_clip import load_open_clip
-        from .japanese_clip import load_japanese_clip
-        from .colxlip import load_colxlip
+       
         return load_func(model_name=model_name, pretrained=pretrained, cache_dir=cache_dir, device=device)
